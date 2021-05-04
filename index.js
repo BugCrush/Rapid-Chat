620ac4755c91c15de2508fa60b3d62e339235ce3
Aditya Khattar <khattaraditya2001@users.noreply.github.com>
2021-05-05 04:05:09 +0530

    Added root folder

---
+++ index.js
@@ -0,0 +1,38 @@
+//Node Server for handling socket.io connections
+
+const express = require('express')
+
+const app = express()
+
+const server = require('http').Server(app)
+
+const io = require('socket.io')(server)
+
+const users = {}
+
+app.use(express.static('public'))
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + "/" + "index.html");
+})
+
+io.on('connection', socket =>{
+
+    socket.on('new-member-logon', name =>{
+        console.log("New member: ", name);
+        users[socket.id] = name;
+        socket.broadcast.emit('member-logon', name);
+    });
+
+    socket.on('msg-send', message =>{
+        socket.broadcast.emit('msg-receive', {message: message, name: users[socket.id]})
+    });
+
+    socket.on('disconnect', message =>{
+        socket.broadcast.emit('member-logout', users[socket.id]);
+        delete users[socket.id];
+    });
+
+})
+
+server.listen(3000)
---
+++ public/js/client.js
@@ -0,0 +1,82 @@
+const socket = io("http://localhost:3000");
+
+const form = document.getElementById('sendmsg');
+
+const msgInp = document.getElementById('msgInp');
+
+const chatArea = document.querySelector(".chat-area");
+
+const name = prompt("Enter your name to start chatting:");
+
+var audioRecieve = new Audio('notif-receive.mp3');
+var audioJoin = new Audio('notif-join.mp3');
+var audioLeft = new Audio('notif-left.mp3');
+
+const append = (message, type, position)=>{
+    const newJoinee = document.createElement('div');
+    newJoinee.innerText = message;
+    newJoinee.classList.add(type);
+    newJoinee.classList.add(position);
+    chatArea.append(newJoinee);
+
+    if(position == 'left'){
+        audioRecieve.play();
+    }
+
+    if(position == 'newJoinee'){
+        audioJoin.play();
+    }
+
+    if(position == 'member-logout'){
+        audioLeft.play();
+    }
+
+    chatArea.scrollTop = chatArea.scrollHeight;
+
+}
+
+socket.emit('new-member-logon', name);
+
+socket.on('member-logon', name =>{
+
+    const welcomeMsgArray = [
+        `${name} has been connected!`,
+        `${name} just landed!`,
+        `${name} just arrrived!`,
+        `${name} slid into the server!`,
+        `${name} is finally here!`,
+        `${name} broke in here!`
+    ]
+    
+    const randomWelcomeMsg = welcomeMsgArray[Math.floor(Math.random()*welcomeMsgArray.length)];    
+
+    append(randomWelcomeMsg, 'new-joinee', 'new-joinee');
+})
+
+socket.on('msg-receive', data =>{
+    append(`${data.name}: ${data.message}`, 'text', 'left');
+})
+
+socket.on('member-logout', name =>{
+
+    const disconnectMsgArray = [
+        `${name} got disconnected`,
+        `${name} left the chat`,
+        `${name} left the conversation`,
+        `${name} is no more... in the chat`,
+        `Pheww! ${name} finally left!`,
+        `You finally got rid of ${name}!`
+    ]
+
+    const randomDisconnectMsg = disconnectMsgArray[Math.floor(Math.random()*disconnectMsgArray.length)];
+
+    append(randomDisconnectMsg, 'member-logout', 'member-logout');
+})
+
+form.addEventListener('submit', (e) =>{
+    e.preventDefault();
+    const message = msgInp.value;
+    append(`You: ${message}`, 'text', 'right');
+    socket.emit('msg-send', message);
+    document.getElementById('msgInp').value = '';
+})
